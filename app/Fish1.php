5d487f1ce7b93486bc98acbdef6147412545a0fe
fahid <fahid@Fahad>
2016-09-01 13:09:35 +0500

    Add Model Classes

---
+++ app/Fish1.php
@@ -0,0 +1,20 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Fish1 extends Model {
+    
+    // MASS ASSIGNMENT -------------------------------------------------------
+    // define which attributes are mass assignable (for security)
+    // we only want these 3 attributes able to be filled
+    protected $fillable = array('weight', 'bear_id');
+
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of fish isnt what we named our database table we have to define it
+    protected $table = 'fish1';
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    public function bear() {
+        return $this->belongsTo('App\Bear');
+    }
+
+}
